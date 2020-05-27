247b8e17a3907d39e1e1fe9ad57f63f3063718f6
Andrews2004000 <57257154+Andrews2004000@users.noreply.github.com>
2020-05-27 21:39:33 +0200

    Prods

--- back/src/routes/Products.ts
+++ back/src/routes/Products.ts
@@ -1,11 +1,11 @@
 import * as ProductsController from '../controllers/Products';
 import ExpressPromiseRouter from 'express-promise-router';
 const router = ExpressPromiseRouter();
-router.get('/prod', ProductsController.createProduct);
-router.get('/prod:prodId', ProductsController.getProduct);
-router.get('/prod', ProductsController.getProducts);
-router.delete('/prod:prodId', ProductsController.deleteProduct);
-router.patch('/prod:prodId', ProductsController.updateProduct);
+router.get('/', ProductsController.createProduct);
+router.get('/:prodId', ProductsController.getProduct);
+router.get('/', ProductsController.getProducts);
+router.delete('/:prodId', ProductsController.deleteProduct);
+router.patch('/:prodId', ProductsController.updateProduct);
 ///Search
 router.get('/prods', ProductsController.Search);
 ////

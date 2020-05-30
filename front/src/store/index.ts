e932d929eb0ccb69fc8ca9ac4f582032f1da5489
Albertoice <alberto@MacBook-Air-di-Alberto.local>
2020-05-30 19:19:54 +0200

    Fixed bug frontend

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,39 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {Api} from './api'
+import { Api } from './api'
 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    prods:[]
+    prods: []
   },
   mutations: {
-    SET_PRODUCTS(state,products){
+    SET_PRODUCTS(state, products) {
       state.prods = products
-      
+
     },
 
-   
-  
+
+
   },
   actions: {
-    async setProducts({commit}){
-    const products =  await Api.fetchData('products')
-      commit('SET_PRODUCTS',products)
+    async setProducts({ commit }) {
+      // const products = await Api.fetchData('products') PRIMA ERA COSÌ
+      const { products } = await Api.fetchData('products')
+      commit('SET_PRODUCTS', products)
 
     },
-    async Login({commit},userCredentilas){
-      const User = await Api.fetchData('user/login',userCredentilas)
-   
+    async Login({ commit }, userCredentilas) {
+      const User = await Api.fetchData('user/login', userCredentilas)
+
     }
-   
-    
-},
-  getters:{
-  
-    
+
+
+  },
+  getters: {
+
+
   },
   modules: {
   }

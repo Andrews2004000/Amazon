e3e96e5a7842c8b79f5170bda70fd2c3bbf32ed0
Albertoice <alberto@MacBook-Air-di-Alberto.local>
2020-06-07 16:21:43 +0200

    Fixed bug

--- front/src/components/Sell.vue
+++ front/src/components/Sell.vue
@@ -1,276 +1,308 @@
 <template>
     <div>
         <div>
-
-        
-        <v-app-bar app class="indigo nav">
-            <span class="fa fa-times cross" router-view to="/"></span>
-         <v-spacer></v-spacer>
-           <v-toolbar-title>
-               <span class="rapha orange--text seller">Seller</span>
-              
-               </v-toolbar-title>
-               <v-spacer></v-spacer>
- 
-
-        </v-app-bar>
+            <v-app-bar app class="indigo nav">
+                <span class="fa fa-times cross" router-view to="/"></span>
+                <v-spacer></v-spacer>
+                <v-toolbar-title>
+                    <span class="rapha orange--text seller">Seller</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+            </v-app-bar>
         </div>
         <div>
- <span class="tes">Create Your Products</span>
+            <span class="tes">Create Your Products</span>
         </div>
         <div>
             <v-form>
-           <v-container>
-               <v-row>
-
-       <v-col cols="12" sm="12">
-        <v-text-field
-        outlined
-              label="Title"
-              placeholder="Title"
-              v-model="userInputs.title"
-              :rules="inputUser"
-            
-            ></v-text-field>
-            </v-col>
-                   <v-col cols="12" sm="12">
-        <v-textarea
-            outlined
-            placeholder="Description"
-            v-model="userInputs.description"
-            :rules="inputDescription"
-            name="input-7-4"
-            label="Description"
-            
-          ></v-textarea>
-          
-            </v-col>
-                   <v-col cols="12" sm="12">
-        <v-text-field
-        outlined
-              label="ImageUrl"
-              placeholder="ImageUrl"
-              v-model="userInputs.image"
-              :rules="imageUrl"
-            
-            ></v-text-field>
-            </v-col>
-                   <v-col cols="12" sm="12">
-        <v-text-field
-        outlined
-              label="Price"
-              placeholder="Price"
-            v-model="userInputs.price"
-
-            ></v-text-field>
-           
-            </v-col>
-              <v-col cols="6" sm="12">
-         <v-select
-            v-model="userInputs.colorsAvailable"
-            :items="item"
-            label="Colors Available"
-            multiple
-            chips
-            hint="Choose Colors You Have"
-            persistent-hint
-          ></v-select>
-           
-            </v-col>
-             <v-col cols="6" sm="12">
-         <v-select
-            
-            :items="size"
-             v-model="userInputs.size"
-            label="Size Available"
-            multiple
-            chips
-            hint="Choose Colors You Have"
-            persistent-hint
-          ></v-select>
-           
-            </v-col>
-          
-              <v-col cols="12" sm="12">
-         <v-select
-            :items="items"
-            label="Category"
-             v-model="userInputs.category"
-            placeholder="Category"
-            outlined
-          ></v-select>
-           
-            </v-col>
-            <v-col cols="12" sm="12">
-         <v-select
-            :items="tags"
-            v-model="userInputs.tags"
-            label="Tags"
-            placeholder="Tags"
-            outlined
-          ></v-select>
-           
-            </v-col>
-
-
-            
-             <v-col cols="12" sm="12">
-        <v-text-field
-        outlined
-              label="Max Quantity"
-              placeholder="Max Quantity"
-              v-model="userInputs.maxQuantity"
-              :rules="[MaxQuantity]"
-              
-            
-            ></v-text-field>
-           
-            </v-col>
-             <v-col cols="12">
-                    <v-menu>
-                        <template v-slot:activator="{ on }">
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="12">
                             <v-text-field
-                               
-                                v-on="on"
-                                label="Due Date"
-                                prepend-icon="date_range"
-                                readonly
-                                :rules="notEmptyRule"
-                                
-                                v-model="userInputs.dates"
+                                outlined
+                                label="Title"
+                                placeholder="Title"
+                                v-model="userInputs.title"
+                                :rules="inputUser"
                             ></v-text-field>
-                        </template>
-                        <v-date-picker  range no-title scrollable></v-date-picker>
-                      <v-spacer></v-spacer>
-                        <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-                            <v-btn text color="primary">OK</v-btn>
-                    </v-menu>
-                    
-                    </v-col>
-                      <v-col cols="12" sm="12">
-            <span class="ratings"> Add Ratings</span>
-         <v-rating v-model="userInputs.ratings"></v-rating>
-           
-            </v-col>
-             </v-row>
-             </v-container>
+                        </v-col>
+                        <v-col cols="12" sm="12">
+                            <v-textarea
+                                outlined
+                                placeholder="Description"
+                                v-model="userInputs.description"
+                                :rules="inputDescription"
+                                name="input-7-4"
+                                label="Description"
+                            ></v-textarea>
+                        </v-col>
+                        <v-col cols="12" sm="12">
+                            <v-text-field
+                                outlined
+                                label="ImageUrl"
+                                placeholder="ImageUrl"
+                                v-model="userInputs.image"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="12">
+                            <v-text-field
+                                outlined
+                                label="Price"
+                                placeholder="Price"
+                                v-model="userInputs.price"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="6" sm="12">
+                            <v-select
+                                v-model="userInputs.colorsAvailable"
+                                :items="item"
+                                label="Colors Available"
+                                multiple
+                                chips
+                                hint="Choose Colors You Have"
+                                persistent-hint
+                            ></v-select>
+                        </v-col>
+                        <v-col cols="6" sm="12">
+                            <v-select
+                                :items="size"
+                                v-model="userInputs.size"
+                                label="Size Available"
+                                multiple
+                                chips
+                                hint="Choose Colors You Have"
+                                persistent-hint
+                            ></v-select>
+                        </v-col>
+
+                        <v-col cols="12" sm="12">
+                            <v-select
+                                :items="items"
+                                label="Category"
+                                v-model="userInputs.category"
+                                placeholder="Category"
+                                outlined
+                            ></v-select>
+                        </v-col>
+                        <v-col cols="12" sm="12">
+                            <v-select
+                                :items="tags"
+                                v-model="userInputs.tags"
+                                label="Tags"
+                                placeholder="Tags"
+                                outlined
+                            ></v-select>
+                        </v-col>
+
+                        <v-col cols="12" sm="12">
+                            <v-text-field
+                                outlined
+                                label="Max Quantity"
+                                placeholder="Max Quantity"
+                                v-model="userInputs.maxQuantity"
+                                :rules="[MaxQuantity]"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-menu
+                                ref="menu"
+                                v-model="isDateMenuOpen"
+                                :close-on-content-click="false"
+                                :return-value.sync="userInputs.scadenza"
+                                transition="scale-transition"
+                                offset-y
+                                min-width="290px"
+                            >
+                                <template v-slot:activator="{ on }">
+                                    <v-text-field
+                                        v-model="userInputs.scadenza"
+                                        label="Sell-by date"
+                                        prepend-icon="event"
+                                        readonly
+                                        v-on="on"
+                                        :rules="notEmptyRule"
+                                    ></v-text-field>
+                                </template>
+                                <v-date-picker v-model="userInputs.scadenza" no-title scrollable>
+                                    <v-spacer></v-spacer>
+                                    <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+                                    <v-btn
+                                        text
+                                        color="primary"
+                                        @click="$refs.menu.save(userInputs.scadenza)"
+                                    >OK</v-btn>
+                                </v-date-picker>
+                            </v-menu>
+                        </v-col>
+                        <v-col cols="12" sm="12">
+                            <span class="ratings">Add Ratings</span>
+                            <v-rating v-model="userInputs.ratings"></v-rating>
+                        </v-col>
+                    </v-row>
+                </v-container>
             </v-form>
-            <v-btn class="yellow btn" @submit="submit">Post Your Product</v-btn>
+            <v-btn class="yellow btn" @click="submit">Post Your Product</v-btn>
         </div>
-       
     </div>
 </template>
 <script>
-import {mapState,mapActions} from 'vuex'
+import { mapState, mapActions } from "vuex";
 export default {
-  data(){
-    return{
-      userInputs:{
-        title:'',
-        description:'',
-        image:'',
-price:'',
-colorsAvailable:'',
-size:'',
-category:'',
-tags:'',
-maxQuantity:'',
-dates:'',
-        ratings:0,
+    data() {
+        return {
+            userInputs: {
+                title: "",
+                description: "",
+                image: "",
+                price: "",
+                colorsAvailable: "",
+                size: "",
+                category: "",
+                tags: "",
+                maxQuantity: "",
+                scadenza: "",
+                ratings: 0
+            },
+            isDateMenuOpen: false,
 
+            notEmptyRule: [v => !!v || "This field is required"],
 
-      },
-    
-       notEmptyRule: [v => !!v || "This field is required"],
-     
-     
-      
-      size:[10,11,12,13,14,15,16,17,18,19,20,
-21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-41,42,43,44,45,46,47,48,49,50],
-      items:['Tecnology','House','Book'],
-      tags:["videogames","phones","computers","fantasy","action","history","livingroom","garden","bedroom"],
-      item:['Red','Blue','Orange','Black','White','Green','Purple','Violet','AzulMarine','pink'],
-       inputUser:[
-            v => v.length >= 3 || 'Minimun Length is 3 Characthers'
-        ],
-        EmailUser:[
-            v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-        ],
-        imageUrl:[
-          v => !v || /\.(gif|jpe?g|tiff|png|webp|bmp)$/i.test(v) || 'Image Not valid'
-
-        ],
-        inputPassword:[
-            v => v.length >= 4 || 'Minimun Length is 4 Characthers'
-        ],
-        inputDescription:[
-          v => v.length >= 15 || 'Minimun Length is 15 Characthers'
-
-        ],
-        MaxQuantity: v=>{
-         if (!v.trim()) return true;
-      if (!isNaN(parseFloat(v)) && v >= 1 && v <= 20) return true;
-      return 'Number has to be between 1 and 20';
-        }
-      
-
-    }
-  
-  },
-    methods:{
-     ...mapActions(['CreteNewProducts']),
-async submit(){
-  console.log('1')
-  await this.CreteNewProducts(this.userInputs)
-  console.log('2')
-  this.$router.push('/')
-
- 
-  
-}
-
-     
-
-      
+            size: [
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+                23,
+                24,
+                25,
+                26,
+                27,
+                28,
+                29,
+                30,
+                31,
+                32,
+                33,
+                34,
+                35,
+                36,
+                37,
+                38,
+                39,
+                40,
+                41,
+                42,
+                43,
+                44,
+                45,
+                46,
+                47,
+                48,
+                49,
+                50
+            ],
+            items: ["Tecnology", "House", "Book"],
+            tags: [
+                "videogames",
+                "phones",
+                "computers",
+                "fantasy",
+                "action",
+                "history",
+                "livingroom",
+                "garden",
+                "bedroom"
+            ],
+            item: [
+                "Red",
+                "Blue",
+                "Orange",
+                "Black",
+                "White",
+                "Green",
+                "Purple",
+                "Violet",
+                "AzulMarine",
+                "pink"
+            ],
+            inputUser: [
+                v => v.length >= 3 || "Minimun Length is 3 Characthers"
+            ],
+            EmailUser: [
+                v =>
+                    !v ||
+                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+                    "E-mail must be valid"
+            ],
+            // imageUrl: [
+            //     v =>
+            //         !v ||
+            //         /\.(gif|jpe?g|tiff|png|webp|bmp)$/i.test(v) ||
+            //         "Image Not valid"
+            // ],
+            inputPassword: [
+                v => v.length >= 4 || "Minimun Length is 4 Characthers"
+            ],
+            inputDescription: [
+                v => v.length >= 15 || "Minimun Length is 15 Characthers"
+            ],
+            MaxQuantity: v => {
+                if (!v.trim()) return true;
+                if (!isNaN(parseFloat(v)) && v >= 1 && v <= 20) return true;
+                return "Number has to be between 1 and 20";
+            }
+        };
     },
-    computed:{
-       ...mapState(["userData"]),
-   
+    methods: {
+        ...mapActions(["CreteNewProducts"]),
+        async submit() {
+            console.log("1");
+            await this.CreteNewProducts(this.userInputs);
+            console.log("2");
+            this.$router.push("/");
+        }
+    },
+    computed: {
+        ...mapState(["userData"])
     }
-    
-}
+};
 </script>
 <style scoped>
-.cross{
-    margin-right:4px;
+.cross {
+    margin-right: 4px;
     color: white;
-    font-size:25px;
+    font-size: 25px;
     cursor: pointer;
 }
-.ratings{
-  text-transform: uppercase;
-  font-weight: 300;
-  font-size:30px;
-
-}
-.text{
-    font-size:20px;
-}
-.seller{
-text-transform:uppercase;
-font-weight:600;
-
-}
-.tes{
-    margin-left:570px;
-    font-size:30px;
+.ratings {
     text-transform: uppercase;
-    font-weight:700;
-
+    font-weight: 300;
+    font-size: 30px;
+}
+.text {
+    font-size: 20px;
+}
+.seller {
+    text-transform: uppercase;
+    font-weight: 600;
+}
+.tes {
+    margin-left: 570px;
+    font-size: 30px;
+    text-transform: uppercase;
+    font-weight: 700;
 }
 
-.btn{
-    margin-left:640px;
+.btn {
+    margin-left: 640px;
 }
 </style>
